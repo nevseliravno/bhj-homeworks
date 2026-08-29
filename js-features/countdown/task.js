14bf23101f6d1f33e9d4f74d78cc39948927c414
sid <sid-ellada@mail.ru>
2026-08-29 18:56:54 +0300

    countdown 29.08

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,1 +1,14 @@
-//
+let element = document.getElementById('timer')
+
+if (element){
+    let time = Number(element.textContent);
+    const intervalId = setInterval(() => {
+        time--;
+        element.textContent = time;
+
+        if(time === 0){
+            clearInterval(intervalId);
+            alert('Вы победили в конкурсе!');
+        }
+    },1000)
+}
