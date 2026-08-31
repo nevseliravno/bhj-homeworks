baf89d53ff085f11efbdc29c598601732157efdc
sid <sid-ellada@mail.ru>
2026-08-31 19:43:20 +0300

    31.08

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,1 +1,17 @@
-.
+let cookie = document.getElementById('cookie');
+
+cookie.addEventListener('click', () => {
+    let countElement = document.getElementById('clicker__counter');
+    let counter = Number(countElement.textContent);
+
+    counter++
+    countElement.textContent = counter;
+
+    if(counter % 2){
+       cookie.width += 100;
+       cookie.height += 64;
+    } else {
+       cookie.width -= 100;
+       cookie.height -= 64;
+    }
+})
